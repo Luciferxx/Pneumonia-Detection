732868716e030d5c7f8cf643e53c5ff5bd2b82a5
Saurabh Kumar Suryan <saurabhsuryan99@hotmail.com>
2020-08-14 11:28:22 +0530

    👍 fixed issues related to parsing file, and added Procfile, requirements.txt, and .gitignore

--- predict.py
+++ predict.py
@@ -2,15 +2,20 @@ import cv2
 import numpy as np
 import requests
 import json
+
 def pre_process(img):
-    img=cv2.imread(img)
     resized = cv2.resize(img,(180,180))
     resized = np.array(resized,dtype=np.float32)
     resized= resized.reshape(-1,180,180,3)
     resized=resized/255
     return resized
+
 def predict(img):
+    
+    npimg = np.fromstring(img, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
     img=pre_process(img)
+
     MODEL_URL = "https://saved-model1.herokuapp.com/v1/models/saved_model1:predict"
     data = json.dumps({
     "signature_name": "serving_default",
